e6de0505a1343fb8fadb3f902bd725ec2f254890
Shell <h.k.domingo@outlook.com>
2022-05-01 14:26:09 +0800

    “Done”

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+    return programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    return programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -81,7 +82,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+   }
+      return programmer_hash
 end
 
 def changing_alan
@@ -103,6 +108,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+
+    return programmer_hash
 
 end
 
@@ -125,5 +133,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    return programmer_hash
 end
